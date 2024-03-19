08b74207e4a59e0bcb18a77acd173d908f3c883b
Daniel Kimolo <dankimist@gmail.com>
2024-03-19 08:42:18 +0300

    Base skeleton

---
+++ src/main/java/com/kinduberre/kindupay/KinduPayApplication.java
@@ -0,0 +1,13 @@
+package com.kinduberre.kindupay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinduPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinduPayApplication.class, args);
+	}
+
+}
