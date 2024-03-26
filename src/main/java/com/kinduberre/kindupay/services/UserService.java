5045e4703139da6a51c64255de97e2b6e6fdc7f3
Daniel Kimolo <dankimist@gmail.com>
2024-03-26 16:29:27 +0300

    Worked on Auth

--- src/main/java/com/kinduberre/kindupay/services/UserService.java
+++ src/main/java/com/kinduberre/kindupay/services/UserService.java
@@ -1,9 +1,13 @@
 package com.kinduberre.kindupay.services;
 
 import com.kinduberre.kindupay.models.auth.User;
+import com.kinduberre.kindupay.models.dtos.familybank.RegistrationDto;
+import com.kinduberre.kindupay.models.dtos.familybank.TokenRequest;
 import com.kinduberre.kindupay.repositories.auth.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,4 +24,33 @@ public class UserService {
         users = new ArrayList<>(userRepository.findAll());
         return users;
     }
+
+    public User saveUser(RegistrationDto userRequest) {
+        if(userRequest.getClientId() == null){
+            throw new RuntimeException("Parameter client_id is not found in request..!!");
+        } else if(userRequest.getClientSecret() == null){
+            throw new RuntimeException("Parameter client_secret is not found in request..!!");
+        }
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = userRequest.getClientSecret();
+        String encodedPassword = encoder.encode(rawPassword);
+
+        User savedUser = new User();
+
+        if(userRepository.findByUsername(userRequest.getClientId()).isEmpty()){
+            User user = new User();
+            user.setPassword(encodedPassword);
+            user.setUsername(userRequest.getClientId());
+            user.setFullName(userRequest.getFullName());
+
+            savedUser = userRepository.save(user);
+        } else {
+                throw new RuntimeException("Client with client_id: " + userRequest.getClientId() + " already exists");
+            }
+
+        return savedUser;
+    }
+
+
 }
